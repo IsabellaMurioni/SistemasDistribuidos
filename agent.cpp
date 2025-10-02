562dd314725e46ba55e72185ae241392e5162e41
capulinGOAT <150143665+crocco76@users.noreply.github.com>
2025-10-01 22:09:02 -0300

    start server

--- agent.cpp
+++ agent.cpp
@@ -1,5 +1,7 @@
+#include "common/tcp_connection.h"
 int main() {
-    // GLHF ;)
-
+    // GLHF 
+    net::TcpConnection conecction  ;
+    conecction.connect("192.168.56.1" ,8080);
     return 0;
 }
---
+++ server.cpp
@@ -0,0 +1,8 @@
+#include "common/tcp_connection.h"
+int main() {
+    // GLHF 
+    net::TcpServer servidor  ;
+        servidor.start();
+
+    return 0;
+}
